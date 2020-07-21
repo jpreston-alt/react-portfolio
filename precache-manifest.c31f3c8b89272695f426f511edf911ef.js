fd0ad6719266c809bf5d7b9043eefff247040089
Joanna Preston <joannappreston@gmail.com>
2020-07-21 12:47:46 -0700

    Updates

---
+++ precache-manifest.c31f3c8b89272695f426f511edf911ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0a3cde488d3040e62a73e806ed11a4c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "589cd33d7e8348de4947",
+    "url": "/react-portfolio/static/css/main.8c13d016.chunk.css"
+  },
+  {
+    "revision": "e311b8da0f359939e4aa",
+    "url": "/react-portfolio/static/js/2.2eb4b2d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.2eb4b2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "589cd33d7e8348de4947",
+    "url": "/react-portfolio/static/js/main.0a8f206c.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
