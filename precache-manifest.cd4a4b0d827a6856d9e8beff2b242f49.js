6686b6835facecd5dad72e60fbdcf2d9737927af
Joanna Preston <joannappreston@gmail.com>
2020-07-21 13:28:47 -0700

    Updates

---
+++ precache-manifest.cd4a4b0d827a6856d9e8beff2b242f49.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddfa64fc1e261a38d7ef2ad69b2e27e8",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "73f8afda5ce6545eacdf",
+    "url": "/react-portfolio/static/css/main.8c13d016.chunk.css"
+  },
+  {
+    "revision": "10be79e441102c847739",
+    "url": "/react-portfolio/static/js/2.cb32954c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.cb32954c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f8afda5ce6545eacdf",
+    "url": "/react-portfolio/static/js/main.24c0edb3.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "2086a850657bd9fee6beb7b216180375",
+    "url": "/react-portfolio/static/media/JoannaPreston_resume.2086a850.pdf"
+  },
+  {
+    "revision": "279d3b3847689d47b506d329be2c6eae",
+    "url": "/react-portfolio/static/media/burger2.279d3b38.png"
+  },
+  {
+    "revision": "e33fed47e7b2b76bd5b2bc2b1538fa98",
+    "url": "/react-portfolio/static/media/check-yourself.e33fed47.png"
+  },
+  {
+    "revision": "2ab8a2fe15dff787f487d22466d4aa61",
+    "url": "/react-portfolio/static/media/dashboard.2ab8a2fe.png"
+  },
+  {
+    "revision": "d4275c6cdcff8caa765895b2e4389151",
+    "url": "/react-portfolio/static/media/emp.d4275c6c.png"
+  },
+  {
+    "revision": "e98dfe8abce938454e342ad2f20ce0bb",
+    "url": "/react-portfolio/static/media/green-leaves-flip.e98dfe8a.jpeg"
+  },
+  {
+    "revision": "cb3011140f88a28bca17dcef058d89d3",
+    "url": "/react-portfolio/static/media/green-leaves.cb301114.jpg"
+  },
+  {
+    "revision": "3a7d07f5dd03f84eac1b483c8917b4e7",
+    "url": "/react-portfolio/static/media/me.3a7d07f5.png"
+  },
+  {
+    "revision": "53049a84a45fcedbf780f72dfc642a61",
+    "url": "/react-portfolio/static/media/notepad2.53049a84.png"
+  },
+  {
+    "revision": "58bfa5fa98ecaa574cb94ebc8e831e6e",
+    "url": "/react-portfolio/static/media/planner.58bfa5fa.png"
+  },
+  {
+    "revision": "ea1ee0d02a70b27b7b7a9832c8bfd3e7",
+    "url": "/react-portfolio/static/media/quar2.ea1ee0d0.png"
+  },
+  {
+    "revision": "a92946c2c94ff40750efb4be0bd89574",
+    "url": "/react-portfolio/static/media/quiz.a92946c2.png"
+  },
+  {
+    "revision": "722e2d1b4eeab6a656db85e94190364a",
+    "url": "/react-portfolio/static/media/team-profile2.722e2d1b.png"
+  },
+  {
+    "revision": "0580e92cdad0224d750b171aab549dba",
+    "url": "/react-portfolio/static/media/top-left.0580e92c.jpeg"
+  },
+  {
+    "revision": "fcc1f30a949cec56d12c0d6e0a85f67c",
+    "url": "/react-portfolio/static/media/weather2.fcc1f30a.png"
+  }
+]);
