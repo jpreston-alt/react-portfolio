32470938c9a0d989241d528e2009eae081fdaf91
Joanna Preston <joannappreston@gmail.com>
2020-07-21 10:47:19 -0700

    skills page

---
+++ src/utils/skillsData.js
@@ -0,0 +1,33 @@
+const clientSide = [
+    "React",
+    "HTML 5",
+    "CSS 3",
+    "JavaScript",
+    "jQuery",
+    "Bootstrap",
+    "Bulma",
+    "UIKit"
+];
+
+const serverSide = [
+    "Node.js",
+    "npm",
+    "Express.js",
+    "Handlebars",
+    "MySQL",
+    "Sequelize",
+    "MongoDB",
+    "AJAX / AXIOS"
+];
+
+const design = [
+    "Figma",
+    "Adobe InDesign",
+    "Adobe Illustrator",
+    "Responsive Web Design",
+    "Sketchup",
+    "AutoCAD"
+];
+
+
+export { clientSide, serverSide, design };
